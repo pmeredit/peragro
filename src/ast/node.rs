4943056949f9a45a69b6be815391ffddf032dd67
Patrick Meredith <pmeredit@gmail.com>
2021-12-07 17:35:10 -0500

    Checkpoint

--- src/ast/node.rs
+++ src/ast/node.rs
@@ -1,24 +1,71 @@
 use std::collections::BTreeMap;
 
-#[derive(Debug, Eq, PartialEq, Ord, PartialOrd)]
+#[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
 pub enum Type {
-    Enum(EnumDefinition),
-    Path(Vec<String>),
+    Enum(Items),
+    Path(String),
     Ptr(Box<Type>),
     Reference(Box<Type>),
-    Struct(StructDefinition),
+    Struct(Items),
     Tuple(Vec<Type>),
-    Opaque(String),
 }
 
-#[derive(Debug, Eq, PartialEq, Ord, PartialOrd)]
-pub struct StructDefinition {
-    generics: Vec<String>,
-    fields: BTreeMap<String, Type>,
+impl Type {
+    pub fn generics(&self) -> Option<&[String]> {
+        match self {
+            Type::Struct(st) => Some(&st.generics),
+            Type::Enum(e) => Some(&e.generics),
+            _ => None,
+        }
+    }
+
+    fn inst(&self, mut theta: BTreeMap<String, String>) -> Self {
+        match self {
+            Type::Enum(e) => Type::Enum(e.inst(theta)),
+            Type::Path(p) => Type::Path(theta.remove(p).unwrap_or_else(|| p.clone())),
+            Type::Ptr(t) => Type::Ptr(Box::new(t.inst(theta))),
+            Type::Reference(t) => Type::Reference(Box::new(t.inst(theta))),
+            Type::Struct(st) => Type::Struct(st.inst(theta)),
+            Type::Tuple(tup) => Type::Tuple(
+                tup.iter()
+                    .cloned()
+                    .map(|ty| ty.inst(theta.clone()))
+                    .collect(),
+            ),
+        }
+    }
+
+    pub fn instantiate(&self, arguments: &[String]) -> Self {
+        match self.generics() {
+            None => self.clone(),
+            Some(g) => {
+                let theta = g
+                    .iter()
+                    .cloned()
+                    .zip(arguments.iter().cloned())
+                    .collect::<BTreeMap<String, String>>();
+
+                self.inst(theta)
+            }
+        }
+    }
 }
 
-#[derive(Debug, Eq, PartialEq, Ord, PartialOrd)]
-pub struct EnumDefinition {
+#[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
+pub struct Items {
     generics: Vec<String>,
-    variants: BTreeMap<String, Type>,
+    items: BTreeMap<String, Type>,
+}
+
+impl Items {
+    fn inst(&self, theta: BTreeMap<String, String>) -> Self {
+        Self {
+            generics: vec![],
+            items: self
+                .items
+                .iter()
+                .map(|(k, v)| (k.clone(), v.inst(theta.clone())))
+                .collect(),
+        }
+    }
 }
